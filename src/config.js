102353bba68ad36aec5b94ce54ab04b8ad6f0b12
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-05 18:14:45 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: {
         //baseURL: "http://localhost:5000"
-        baseURL: "https://backend-admin-jxqq.onrender.com/"
+        baseURL: "https://backend-admin-s7r5.onrender.com"
     }
 }
