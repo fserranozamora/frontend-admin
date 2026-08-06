5b3a8fbd0f949821acc94aebd86e942924ae6459
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-05 21:16:02 -0500

    Remove buttons from EditarClientes component
    Removed collapse and remove buttons from the client edit card.

--- src/paginas/modulos/EditarClientes.js
+++ src/paginas/modulos/EditarClientes.js
@@ -63,22 +63,6 @@ const EditarClientes = () => {
                             <div className="card-header">
                                 <h3 className="card-title">Editar Información de Cliente</h3>
                                 <div className="card-tools">
-                                    <button
-                                        type="button"
-                                        className="btn btn-tool"
-                                        data-card-widget="collapse"
-                                        title="Collapse"
-                                    >
-                                        <i className="fas fa-minus"></i>
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="btn btn-tool"
-                                        data-card-widget="remove"
-                                        title="Remove"
-                                    >
-                                        <i className="fas fa-times"></i>
-                                    </button>
                                 </div>
                             </div>
 
