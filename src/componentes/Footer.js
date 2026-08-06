ae9e43768323eef30ccd60bc939b58ac6f864256
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-05 21:18:34 -0500

    Update footer version from 1.0.5 to 1.0.6

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="main-footer">
             <div className="float-right d-none d-sm-block">
-                <b>Version</b> 1.0.5
+                <b>Version</b> 1.0.6
             </div>
             <strong>Copyright © 2014-2024 <Link to={"https://adminlte.io"}>AdminLTE.io</Link>.</strong> All rights reserved.
         </footer>
