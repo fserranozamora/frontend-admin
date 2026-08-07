d526369541fb0363c5f1a3fefeb9acbf50257a74
fserranozamora <85131139+fserranozamora@users.noreply.github.com>
2026-08-07 18:03:18 -0500

    Update footer version and copyright year

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer className="main-footer">
             <div className="float-right d-none d-sm-block">
-                <b>Version</b> 1.0.6
+                <b>Version</b> 1.0.7
             </div>
-            <strong>Copyright © 2014-2024 <Link to={"https://adminlte.io"}>AdminLTE.io</Link>.</strong> All rights reserved.
+            <strong>Copyright © 2014-2026 <Link to={"https://adminlte.io"}>AdminLTE.io</Link>.</strong> All rights reserved.
         </footer>
     );
 }
